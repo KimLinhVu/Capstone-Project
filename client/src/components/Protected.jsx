3e90973f5d150220dd5de663a15355b3df432fa4
KimLinhVu <klvbubble@gmail.com>
2022-07-08 15:41:38 -0700

    redirects user to login page if they try to access a protected route with an invalid access token

--- client/src/components/Protected.jsx
+++ client/src/components/Protected.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
 import { logout } from '../spotify'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export const Protected = ({token, children}) => {
-  if (!token) {
-    logout()
-    return <Navigate to="/login" />
-  }
+  const navigate = useNavigate()
+  useEffect(() => {
+    axios.post('http://localhost:8888/verify-token', {
+      token: token
+    }).then(res => {
+      console.log(res)
+      if (!res.data.auth) {
+        logout()
+        console.log('here')
+        navigate('/login')
+      }
+    })
+  }, [])
+  
   return children
 }
