238f2e2b4b3ff7c6d9b419b6a47bf8b98ffc6ad3
KimLinhVu <klvbubble@gmail.com>
2022-07-25 14:01:37 -0700

    changed similarity test descriptions and other feedback changes

--- client/src/utils/similarity.test.js
+++ client/src/utils/similarity.test.js
@@ -1,59 +1,59 @@
 const Similarity = require('./similarity.js')
 
-test('Test 0: properly calculates cosine similarity', () => {
+test('returns 0 when track vectors are the same for cosine similarity', () => {
   const a = [0.333, 0.675, 0.616, 0.001, 0.472, 0.161, 0.906, 0.6, 0.089, 0.230, 0.536]
   const b = [0.333, 0.675, 0.616, 0.001, 0.472, 0.161, 0.906, 0.6, 0.089, 0.230, 0.536]
   const similar = new Similarity()
   expect(similar.calculateCosineSimilarity(a, b)).toBe(0)
 })
 
-test('Test 1: properly calculates cosine similarity', () => {
+test('returns a number between 0-100 when track vectors are different for cosine similarity', () => {
   const a = [0.333, 0.675, 0.616, 0.001, 0.472, 0.161, 0.906, 0.6, 0.089, 0.230, 0.536]
   const b = [0.242, 0.730, 0.432, 0.017, 0.490, 0.193, 0.844, 0.6, 0.105, 0.25, 0.415]
   const similar = new Similarity()
   expect(similar.calculateCosineSimilarity(a, b)).toBeCloseTo(9.2762, 4)
 })
 
-test('Test 2: properly calculates cosine similarity', () => {
+test('returns a number between 0-100 when track vectors are different for cosine similarity', () => {
   const a = [0.257, 0.605, 0.621, 0.0004, 0.538, 0.195, 0.896, 0.826, 0.059, 0.250, 0.430]
   const b = [0.368, 0.618, 0.478, 0.005, 0.561, 0.177, 0.859, 0.708, 0.081, 0.25, 0.415]
   const similar = new Similarity()
   expect(similar.calculateCosineSimilarity(a, b)).toBeCloseTo(7.8237, 4)
 })
 
-test('Test 3: properly calculates cosine similarity', () => {
+test('returns 100 when track vectors are completely different for cosine similarity', () => {
   const a = [0.552, 0.461, 0.366, 0.071, 0.468, 0.166, 0.824, 0.7, 0.048, 0.15, 0.383]
-  const b = [0.324, 0.235, 0.742, 0.942, 0.435, 0.436, 0.351, 0.3, 0.845, 0.25, 0.415]
+  const b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   const similar = new Similarity()
-  expect(similar.calculateCosineSimilarity(a, b)).toBeCloseTo(57.6561, 4)
+  expect(similar.calculateCosineSimilarity(a, b)).toBe(100)
 })
 
-test('Test 0: properly calculates own similarity', () => {
+test('returns 0 when track vectors are the same for own similarity', () => {
   const a = [0.333, 0.675, 0.616, 0.001, 0.472, 0.161, 0.906, 0.6, 0.089, 0.230, 0.536]
   const b = [0.333, 0.675, 0.616, 0.001, 0.472, 0.161, 0.906, 0.6, 0.089, 0.230, 0.536]
   const similar = new Similarity()
   expect(similar.calculateOwnSimilarity(a, b)).toBe(0)
 })
 
-test('Test 1: properly calculates own similarity', () => {
+test('returns a number between 0-100 when track vectors are different for own similarity', () => {
   const a = [0.333, 0.675, 0.616, 0.001, 0.472, 0.161, 0.906, 0.6, 0.089, 0.230, 0.536]
   const b = [0.242, 0.730, 0.432, 0.017, 0.490, 0.193, 0.844, 0.6, 0.105, 0.25, 0.415]
   const similar = new Similarity()
   expect(similar.calculateOwnSimilarity(a, b)).toBeCloseTo(17.3879, 4)
 })
 
-test('Test 2: properly calculates own similarity', () => {
+test('returns a number between 0-100 when track vectors are different for own similarity', () => {
   const a = [0.257, 0.605, 0.621, 0.0004, 0.538, 0.195, 0.896, 0.826, 0.059, 0.250, 0.430]
   const b = [0.368, 0.618, 0.478, 0.005, 0.561, 0.177, 0.859, 0.708, 0.081, 0.25, 0.415]
   const similar = new Similarity()
   expect(similar.calculateOwnSimilarity(a, b)).toBeCloseTo(12.2634, 4)
 })
 
-test('Test 3: properly calculates own similarity', () => {
+test('returns 100 when track vectors are completely different for own similarity', () => {
   const a = [0.552, 0.461, 0.366, 0.071, 0.468, 0.166, 0.824, 0.7, 0.048, 0.15, 0.383]
-  const b = [0.324, 0.235, 0.742, 0.942, 0.435, 0.436, 0.351, 0.3, 0.845, 0.25, 0.415]
+  const b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   const similar = new Similarity()
-  expect(similar.calculateOwnSimilarity(a, b)).toBeCloseTo(58.8403, 4)
+  expect(similar.calculateOwnSimilarity(a, b)).toBe(100)
 })
 
 test('test calculate euclidean distance', () => {
--- server/utils/similarity.test.js
+++ server/utils/similarity.test.js
@@ -1,21 +1,21 @@
 const Similarity = require('./similarity')
 
-test('Test 0: propertly returns correct similarity method', () => {
+test('returns 0 when 3-byte inc. counter in mongodb object id is an even number', () => {
   expect(Similarity.getSimilarityMethod('62da14f88d6e09b9d06dace3')).toBe(0)
 })
 
-test('Test 1: propertly returns correct similarity method', () => {
+test('returns 1 when 3-byte inc. counter in mongodb object id is an odd number', () => {
   expect(Similarity.getSimilarityMethod('62dd9b3ea1f736b714589cd8')).toBe(1)
 })
 
-test('Test 2: propertly returns correct similarity method', () => {
+test('returns 0 when 3-byte inc. counter in mongodb object id is an even number', () => {
   expect(Similarity.getSimilarityMethod('62ded3de427cddcfaf0578a4')).toBe(0)
 })
 
-test('Test 3: propertly returns correct similarity method', () => {
+test('returns 1 when 3-byte inc. counter in mongodb object id is an odd number', () => {
   expect(Similarity.getSimilarityMethod('62ded459427cddcfaf0578f7')).toBe(1)
 })
 
-test('Test 4: propertly returns correct similarity method', () => {
+test('returns 0 when 3-byte inc. counter in mongodb object id is an even number', () => {
   expect(Similarity.getSimilarityMethod('62dee472fd22233bb057fcbb')).toBe(0)
 })
