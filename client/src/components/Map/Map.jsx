30be6e1a7db367b62930edcb6073d416b6ed5902
KimLinhVu <klvbubble@gmail.com>
2022-07-25 17:10:39 -0700

    Revert "Merge branch 'similarity' of https://github.com/KimLinhVu/Capstone-Project into similarity"
    This reverts commit cc8ad59095dd3bc02128e3b1dae803d288a2cf0a, reversing
    changes made to 69c76cd44d7c5351c249287fc46c84c2022f4abd.
    
    revert similarity merge

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -15,7 +15,6 @@ function Map({
   setIsLoading
 }) {
   const [map, setMap] = useState(null)
-  const [onLoad, setOnLoad] = useState(true)
   /* show users based on map zoom and not clicking on marker */
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -28,19 +27,6 @@ function Map({
     streetViewControl: false,
     fullscreenControl: false,
     
-  }
-
-  const onBoundsChanged = () => {
-    setIsLoading(true)
-    /* check which markers are currently in view of map bounds */
-    setUsers([])
-    markerArray?.forEach(item => {
-      const inBounds = map?.getBounds()?.contains(item)
-      if (inBounds) {
-        handleMarkerOnClick(item)
-      }
-    })
-    setIsLoading(false)
   }
 
   if (!isLoaded) {
@@ -63,8 +49,6 @@ function Map({
         /* use random similarity method associated with user */
         let similarity = 0
         if (similarityMethod === 0) {
-          console.log(vector)
-          console.log(userVector)
           similarity = similar.calculateCosineSimilarity(vector, userVector)
         } else {
           similarity = similar.calculateOwnSimilarity(vector, userVector)
@@ -76,6 +60,18 @@ function Map({
     })
   }
 
+  const onBoundsChanged = () => {
+    setIsLoading(true)
+    /* check which markers are currently in view of map bounds */
+    markerArray.forEach(item => {
+      const inBounds = map?.getBounds().contains(item)
+      if (inBounds) {
+        handleMarkerOnClick(item)
+      }
+    })
+    setIsLoading(false)
+  }
+
   return (
     <GoogleMap
       zoom={8}
@@ -83,13 +79,6 @@ function Map({
       options={options}
       mapContainerClassName="map-container"
       onZoomChanged={onBoundsChanged}
-      onDragEnd={onBoundsChanged}
-      onBoundsChanged={() => {
-        if (onLoad) {
-          onBoundsChanged()
-          setOnLoad(false)
-        }
-      }}
       onLoad={map => {
         setMap(map)
       }}
