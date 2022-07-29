b48db0e98a7f316bcdea02df62adedd8282c6614
KimLinhVu <klvbubble@gmail.com>
2022-07-28 18:13:26 -0700

    fix userplalist

--- client/src/components/UserPlaylist/UserPlaylist.jsx
+++ client/src/components/UserPlaylist/UserPlaylist.jsx
@@ -7,57 +7,32 @@ function UserPlaylist({
   playlist,
   similarity,
   playlistId,
-  similarityMethod
+  similarityMethod,
+  vector,
+  userVector
 }) {
-  const [isFollowing, setIsFollowing] = useState()
-  let followButton;
 
   const data = {
     similarityMethod: similarityMethod,
-    originalPlaylistId: playlistId
-  }
-
-  if (isFollowing) {
-    followButton = <button className='unfollow' onClick={handleOnClickUnfollow}>Unfollow User</button>
-  } else {
-    followButton = <button className='follow' onClick={handleOnClickFollow}>Follow User</button>
-  }
-
-  useEffect(() => {
-    /* check to see if current user is already following user */
-    const found = following.some(obj => obj.userId === user._id)
-    if (found) {
-      setIsFollowing(true)
-    } else {
-      setIsFollowing(false)
-    }
-  }, [])
-
-  const handleOnClickFollow = async () => {
-    await addUserFollowing(user._id)
-    await addUserFollower(user._id)
-    setIsFollowing(!isFollowing)
-  }
-
-  const handleOnClickUnfollow = async () => {
-    await removeUserFollowing(user._id)
-    await removeUserFollower(user._id)
-    setIsFollowing(!isFollowing)
+    originalPlaylistId: playlistId,
+    user: user,
+    vector: vector,
+    userVector: userVector
   }
 
   return (
-    <div className='user-playlist'>
-      <h2>{user.username}</h2>
-      {followButton}
-      {playlist ? (
-        <div className='playlist-card'>
-          <p>{playlist.name}</p>
-          <p>Similarity Method: {similarityMethod === 0 ? 'Cosine' : 'Own'}</p>
-          <p>Similarity Score: {similarity}</p>
-          <Link to={`/user/${playlist.id}`} state={data}><img src={playlist.images[0].url}/></Link>
+    <Link to={`/user/${playlist.id}`} state={data} style={{textDecoration: 'none'}}><div className='user-playlist'>
+      <div className="left">
+        <img src={playlist.images[0].url}/>
+        <div className="info">
+          <span className='username'>{user.username}</span>
+          <span className="playlist-title">{playlist.name}</span>
         </div>
-      ) : null}
-    </div>
+      </div>
+      <div className="right">
+        <button className='similarity'>{similarity}</button>
+      </div>
+    </div></Link>
   )
 }
 
