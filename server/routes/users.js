1b6533b59c6c86a4aa0ef6a75a27bbc56aa7869c
KimLinhVu <klvbubble@gmail.com>
2022-07-28 21:04:10 -0700

    fix files

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,28 +5,12 @@ const router = express.Router()
 const Users = require('../models/Users')
 const Playlist = require('../models/Playlists')
 
-router.post('/', jwt.verifyJWT, async (req, res, next) => {
+router.get('/', jwt.verifyJWT, async (req, res, next) => {
   try {
     /* gets all users except self */
     const userId = req.userId
-    const { followers } = req.body
-    const allUsers = await Users.find({ _id: { $ne: userId } })
-
-    /* filters out users who are private and are not following current user */
-    const filteredUsers = allUsers.filter(user => {
-      /* check to see if user is private and if user is a follower */
-      if (user.privacy === true) {
-        if (user.showFollowing === true) {
-          const found = followers?.some(obj => obj.userId === user.id)
-          if (found) {
-            return true
-          }
-        }
-        return false
-      }
-      return true
-    })
-    res.json(filteredUsers)
+    const allUsers = await Users.find({ _id: { $ne: userId }, privacy: false })
+    res.json(allUsers)
   } catch (error) {
     next(error)
   }
@@ -67,69 +51,6 @@ router.get('/location', jwt.verifyJWT, async (req, res, next) => {
     const userId = req.userId
     const user = await Users.findOne({ _id: userId })
     res.json(user.location)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/add-follower', jwt.verifyJWT, async (req, res, next) => {
-  try {
-    const userId = req.userId
-    const { otherUserId } = req.body
-    await Users.findOneAndUpdate({ _id: otherUserId }, {
-      $push: {
-        followers: { userId }
-      }
-    })
-    res.status(200).json()
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/add-following', jwt.verifyJWT, async (req, res, next) => {
-  try {
-    const userId = req.userId
-    const { otherUserId } = req.body
-
-    await Users.findOneAndUpdate({ _id: userId }, {
-      $push: {
-        following: { userId: otherUserId }
-      }
-    })
-    res.status(200).json()
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/remove-follower', jwt.verifyJWT, async (req, res, next) => {
-  try {
-    const userId = req.userId
-    const { otherUserId } = req.body
-
-    await Users.findOneAndUpdate({ _id: otherUserId }, {
-      $pull: {
-        followers: { userId }
-      }
-    })
-    res.status(200).json()
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.post('/remove-following', jwt.verifyJWT, async (req, res, next) => {
-  try {
-    const userId = req.userId
-    const { otherUserId } = req.body
-
-    await Users.findOneAndUpdate({ _id: userId }, {
-      $pull: {
-        following: { userId: otherUserId }
-      }
-    })
-    res.status(200).json()
   } catch (error) {
     next(error)
   }
