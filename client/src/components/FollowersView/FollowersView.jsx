00133d4ba38d454212edf8d6c2790d341e8af9c3
KimLinhVu <klvbubble@gmail.com>
2022-07-28 18:11:19 -0700

    fix followersview and users.js

--- client/src/components/FollowersView/FollowersView.jsx
+++ client/src/components/FollowersView/FollowersView.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import UserCard from 'components/UserCard/UserCard'
 import './FollowersView.css'
 import { getUserProfileById } from 'utils/users'
 
-function FollowersView({
+function FollowersView ({
   profile,
   followers,
   setPopupIsOpen,
   setUserPopupId
 }) {
-  const users = followers ? profile.followers : profile.following
-  let userCards;
+  const [followerSearch, setFollowerSearch] = useState('')
+  const [users, setUsers] = useState(null)
+  const [displayUsers, setDisplayUsers] = useState([])
 
-  if (users.length === 0) {
-    userCards = <p>No followers</p>
-  } else {
-    userCards = users.map((item, idx) => {
-      return <UserCard key={idx} userId={item.userId} />
-    })
-  }
+  const userArray = followers ? profile.followers : profile.following
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      let newArray = []
+      const promises = userArray.map(async (item) => {
+        const { data } = await getUserProfileById(item.userId)
+        newArray.push(data)
+      })
+      await Promise.all(promises)
+
+      /* filter out private users */
+      newArray = newArray.filter(user => {
+        if (user.privacy && !user.showFollowing) {
+          return false
+        }
+        return true
+      })
+
+      setUsers(newArray)
+      setDisplayUsers(newArray)
+    }
+    getUserInfo()
+  }, [profile])
+
+  useEffect(() => {
+    const newArray = users?.filter(item => { return item.username.toLowerCase().includes(followerSearch.toLowerCase()) })
+    setDisplayUsers(newArray)
+  }, [followerSearch, profile])
+
   return (
     <div className="followers-view">
       <input type="text" placeholder='Search Users' className='follower-searchbar' value={followerSearch} onChange={(e) => setFollowerSearch(e.target.value)}/>
@@ -28,7 +51,13 @@ function FollowersView({
           <h3>{followers ? `${profile.username}'s Followers` : 'Following'}</h3>
         </div>
         <div className="playlists">
-          {userCards}
+          {displayUsers?.length !== 0
+            ? (
+                displayUsers?.map((item, idx) => {
+                  return <UserCard key={idx} userId={item._id} user={item} setPopupIsOpen={setPopupIsOpen} setUserPopupId={setUserPopupId}/>
+                })
+              )
+            : <p>No Users Found</p>}
         </div>
       </div>
     </div>
