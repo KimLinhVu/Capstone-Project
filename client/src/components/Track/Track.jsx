8cc16a2c1577f34ede13e90f7fc560f21be4027d
KimLinhVu <klvbubble@gmail.com>
2022-07-30 12:18:53 -0700

    fix files

--- client/src/components/Track/Track.jsx
+++ client/src/components/Track/Track.jsx
@@ -9,17 +9,21 @@ function Track ({
   const tracks = new Tracks()
   return (
     <div className="track">
-      <div className="track-left">
-        <span className='num'>{trackNumber + 1}</span>
-        <img src={track.album.images[0]?.url} alt="Track Image"/>
-        <div className="info">
-          <span className='title'>{track.name}</span>
-          <span className='artist'>{track.artists[0].name}</span>
-        </div>
-      </div>
-      <div className="track-right">
-        <span className="time">{tracks.convertDuration(track.duration_ms)}</span>
-      </div>
+      {track && (
+        <>
+          <div className="track-left">
+            <span className='num'>{trackNumber + 1}</span>
+            <img src={track.album.images[0]?.url} alt="Track Image"/>
+            <div className="info">
+              <span className='title'>{track.name}</span>
+              <span className='artist'>{track.artists[0].name}</span>
+            </div>
+          </div>
+          <div className="track-right">
+            <span className="time">{tracks.convertDuration(track.duration_ms)}</span>
+          </div>
+        </>
+      )}
     </div>
   )
 }
