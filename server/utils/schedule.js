cf88b6250bd4a22e816fc438b4a55a323764c9b0
KimLinhVu <klvbubble@gmail.com>
2022-08-07 11:44:33 -0700

    added job that removes expired track receipts every hour

--- server/utils/schedule.js
+++ server/utils/schedule.js
@@ -1,7 +1,26 @@
 const schedule = require('node-schedule')
 const Similarity = require('./similarity')
+const TrackReceipt = require('../models/TrackReceipt')
 
 /* job that updates all similarity scores in database every 5th minute */
 schedule.scheduleJob('*/5 * * * *', async () => {
   await Similarity.updateSimilarityScores()
+})
+
+/* job that removes expired track receipts at the start of every hour */
+schedule.scheduleJob('0 * * * *', async () => {
+  const receipts = await TrackReceipt.find()
+
+  /* delete expired receipts */
+  const promises = receipts.map(async (receipt) => {
+    const receiptDate = new Date(receipt.addedAt)
+    const receiptSeconds = receiptDate.getTime() / 1000
+    const timeNow = Date.now() / 1000
+
+    /* delete if longer than 7 days */
+    if ((timeNow - receiptSeconds) > 604800) {
+      await TrackReceipt.findOneAndRemove({ _id: receipt._id })
+    }
+  })
+  await Promise.all(promises)
 })
