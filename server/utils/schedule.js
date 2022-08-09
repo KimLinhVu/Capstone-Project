aece54972e4e41de9031ff95d752bf478cf439d9
KimLinhVu <klvbubble@gmail.com>
2022-08-09 09:36:00 -0700

    update track receipts to addedTrack

--- server/utils/schedule.js
+++ server/utils/schedule.js
@@ -1,25 +1,25 @@
 const schedule = require('node-schedule')
 const Similarity = require('./similarity')
-const TrackReceipt = require('../models/TrackReceipt')
+const addedTrack = require('../models/addedTrack')
 
 /* job that updates all similarity scores in database every 5th minute */
 schedule.scheduleJob('*/5 * * * *', async () => {
   await Similarity.updateSimilarityScores()
 })
 
-/* job that removes expired track receipts at the start of every hour */
+/* job that removes expired track records at the start of every hour */
 schedule.scheduleJob('0 * * * *', async () => {
-  const receipts = await TrackReceipt.find()
+  const tracks = await addedTrack.find()
 
-  /* delete expired receipts */
-  const promises = receipts.map(async (receipt) => {
-    const receiptDate = new Date(receipt.addedAt)
-    const receiptSeconds = receiptDate.getTime() / 1000
+  /* delete expired tracks */
+  const promises = tracks.map(async (track) => {
+    const trackDate = new Date(track.addedAt)
+    const trackSeconds = trackDate.getTime() / 1000
     const timeNow = Date.now() / 1000
 
     /* delete if longer than 7 days */
-    if ((timeNow - receiptSeconds) > 604800) {
-      await TrackReceipt.findOneAndRemove({ _id: receipt._id })
+    if ((timeNow - trackSeconds) > 604800) {
+      await addedTrack.findOneAndRemove({ _id: track._id })
     }
   })
   await Promise.all(promises)
