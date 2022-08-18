f7e0ac20f35c5958794913e1fb2e31af6b4105c8
KimLinhVu <klvbubble@gmail.com>
2022-08-18 10:25:45 -0700

    add env variable for rapidapi key

--- server/utils/shazam.js
+++ server/utils/shazam.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
+require('dotenv').config()
 
 const getTrackDetails = (trackSearchParams) => {
   const uri = encodeURI(`https://shazam.p.rapidapi.com/search?term=${trackSearchParams}&locale=en-US&offset=0&limit=1`)
   return axios.get(uri, {
     headers: {
-      'X-RapidAPI-Key': '1731bc1cc2mshbe69949fd2ac43cp1db541jsncfade678c239',
+      'X-RapidAPI-Key': process.env.X_RAPIDAPI_KEY,
       'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
     }
   })
